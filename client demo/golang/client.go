24c04d8464d2b62f3cc6d6628db52b32e33fe43f
ConanYu <C.zing@outlook.com>
2021-09-29 20:14:00 +0800

    feat: client demo

---
+++ client demo/golang/client.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"goodguy_crawl_client_demo/pb"
+
+	"google.golang.org/grpc"
+)
+
+func GetClient() (*grpc.ClientConn, *pb.CrawlServiceClient, error) {
+	conn, err := grpc.Dial(
+		fmt.Sprintf("%s:%d", "localhost", 50050),
+		grpc.WithInsecure(),
+		grpc.WithBlock(),
+	)
+	if err != nil {
+		return conn, nil, err
+	}
+	client := pb.NewCrawlServiceClient(conn)
+	return conn, &client, nil
+}
+
+func main() {
+	conn, client, err := GetClient()
+	defer func(conn *grpc.ClientConn) {
+		err := conn.Close()
+		if err != nil {
+			fmt.Printf("close grpc connection fail, err=%v\n", err)
+		}
+	}(conn)
+	if err != nil {
+		fmt.Printf("get grpc client fail, err=%v\n", err)
+		return
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+	platform := "codeforces"
+	rsp, err := (*client).GetRecentContest(ctx, &pb.GetRecentContestRequest{
+		Platform: platform,
+	})
+	if err != nil {
+		fmt.Println("get recent contest failed. platform=%s, err=%v\n", platform, err.Error())
+		return
+	}
+	fmt.Println(*rsp)
+}
