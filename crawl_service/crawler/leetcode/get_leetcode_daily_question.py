f152d568ec5d8be02a2f6ad5e9d82cc91be925a4
zmm <2191482447@qq.com>
2022-05-01 19:21:47 +0800

    fix: change leetcode domain

--- crawl_service/crawler/leetcode/get_leetcode_daily_question.py
+++ crawl_service/crawler/leetcode/get_leetcode_daily_question.py
@@ -16,16 +16,16 @@ def get_leetcode_daily_question() -> crawl_service_pb2.GetDailyQuestionResponse:
     headers = {
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
                       "Chrome/79.0.3945.130 Safari/537.36",
-        "x-csrftoken": get_leetcode_csrf_token(session, 'https://leetcode-cn.com/contest/'),
-        "origin": 'https://leetcode-cn.com',
-        "referer": 'https://leetcode-cn.com/contest/',
+        "x-csrftoken": get_leetcode_csrf_token(session, 'https://leetcode.cn/contest/'),
+        "origin": 'https://leetcode.cn',
+        "referer": 'https://leetcode.cn/contest/',
         "Connection": 'keep-alive',
         "Content-Type": 'application/json',
     }
     result = RequestExecutorManage.work(
         'leetcode',
         session.post,
-        'https://leetcode-cn.com/graphql',
+        'https://leetcode.cn/graphql',
         data=data,
         headers=headers,
     ).json()['data']['todayRecord']
@@ -34,7 +34,7 @@ def get_leetcode_daily_question() -> crawl_service_pb2.GetDailyQuestionResponse:
         q = r['question']
         problem.append(crawl_service_pb2.GetDailyQuestionResponse.Problem(
             platform='leetcode',
-            url=f'https://leetcode-cn.com/problems/{q["titleSlug"]}/',
+            url=f'https://leetcode.cn/problems/{q["titleSlug"]}/',
             id=q['questionId'],
             name=q['titleCn'],
             difficulty=q['difficulty']
