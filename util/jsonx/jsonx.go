d61280f51e5f550e41338874ffb0d3167a00cf81
ConanYu <33613505+ConanYu@users.noreply.github.com>
2023-03-25 01:01:49 +0800

    feat: sonic use self-definition client

--- util/jsonx/jsonx.go
+++ util/jsonx/jsonx.go
@@ -6,32 +6,40 @@ import (
 	"github.com/bytedance/sonic"
 )
 
+var (
+	encoder = sonic.Config{}.Froze()
+)
+
 func Marshal(val any) []byte {
-	r, _ := sonic.Marshal(val)
+	r, _ := encoder.Marshal(val)
 	return r
 }
 
 func MarshalString(val any) string {
-	r, _ := sonic.MarshalString(val)
+	r, _ := encoder.MarshalToString(val)
 	return r
 }
+
+var (
+	decoder = sonic.Config{UseInt64: true}.Froze()
+)
 
 func Unmarshal[Resp any, Req string | []byte](s Req) (resp Resp, err error) {
 	if typeof := reflect.TypeOf(resp); typeof.Kind() == reflect.Pointer {
 		resp = reflect.New(typeof.Elem()).Interface().(Resp)
 		switch s := any(s).(type) {
 		case string:
-			err = sonic.UnmarshalString(s, resp)
+			err = decoder.UnmarshalFromString(s, resp)
 		case []byte:
-			err = sonic.Unmarshal(s, resp)
+			err = decoder.Unmarshal(s, resp)
 		}
 		return
 	}
 	switch s := any(s).(type) {
 	case string:
-		err = sonic.UnmarshalString(s, &resp)
+		err = decoder.UnmarshalFromString(s, &resp)
 	case []byte:
-		err = sonic.Unmarshal(s, &resp)
+		err = decoder.Unmarshal(s, &resp)
 	}
 	return
 }
